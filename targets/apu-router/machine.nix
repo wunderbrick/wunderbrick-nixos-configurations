b5870b5d13c505d7fe093ce498869d7c3984cc32
wunderbrick <wunderbrick@lambdafuzz.com>
2021-04-13 19:16:47 -0400

    Tweak router (NAT's not working it seems)

--- targets/apu-router/machine.nix
+++ targets/apu-router/machine.nix
@@ -6,23 +6,79 @@
     ../../shared/system-attributes/ssh-strict.nix
   ];
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
-
-  networking.hostName = "apu-router"; # Define your hostname.
-  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  boot = {
+    loader.grub = {
+      enable = true;
+      version = 2;
+      device = "/dev/sda"; # or "nodev" for efi only
+    };
+  };
 
   time.timeZone = "America/New_York";
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp1s0.useDHCP = true;
-  networking.interfaces.enp2s0.useDHCP = true;
-  networking.interfaces.enp3s0.useDHCP = true;
-  networking.interfaces.wlp5s0.useDHCP = true;
+  networking = {
+    hostName = "apu-router";
+    firewall = {
+      enable = false;
+      #allowedTCPPorts = [ 22 ];
+      #trustedInterfaces = [ "wlp5s0" ];
+    };
+    nat = {
+      enable = true;
+      internalIPs = [ "192.168.11.0/24" ];
+      #internalInterfaces = [ "wlp5s0" ];
+      externalInterface = "enp1s0";
+    };
+    useDHCP = false;
+    interfaces = {
+      enp1s0.useDHCP = true;
+      #enp2s0 = {
+      #  ipAddress = "192.168.12.1";
+      #  prefixLength = 24;
+      #};
+      #enp3s0 = {
+      #  ipAddress = "192.168.13.1";
+      #  prefixLength = 24;
+      #};
+      wlp5s0.ipv4 = {
+        addresses = [
+          {
+            address = "192.168.11.1";
+            prefixLength = 24;
+          }
+        ];
+      };
+    };
+  };
+
+  services = {
+    hostapd = {
+      enable = true;
+      interface = "wlp5s0";
+      ssid = "NixOS Cult";
+      wpaPassphrase = "toor toor";
+      hwMode = "g";
+      channel = 10;
+    };
+    dhcpd4 = {
+      enable = true;
+      interfaces = [ "wlp5s0" ];
+      extraConfig = ''
+        option domain-name-servers 1.1.1.1, 8.8.8.8;
+        option subnet-mask 255.255.255.0;
+        option broadcast-address 192.168.11.255;
+        subnet 192.168.11.0 netmask 255.255.255.0 {
+          range 192.168.11.100 192.168.11.200;
+          option subnet-mask 255.255.255.0;
+          option broadcast-address 192.168.11.255;
+          option routers 192.168.11.1
+        }
+      '';
+    };
+  };
 
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
@@ -37,8 +93,6 @@
   };
 
   environment.systemPackages = with pkgs; [ wget htop ];
-
-  networking.firewall.allowedTCPPorts = [ 22 ];
 
   nix.gc = {
     automatic = true;
